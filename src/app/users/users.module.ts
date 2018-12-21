97493bc4974af1a058179ef6f6997f8cb9abfd05
Sergio <sergioh.c@live.com>
2018-12-21 17:31:28 -0200

    adjust module user

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { MaterializeModule } from 'ngx-materialize';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MaterializeModule.forRoot()
+  ],
+  exports: [
+    LoginComponent,
+    RegisterComponent,
   ],
   declarations: [LoginComponent, RegisterComponent]
 })
