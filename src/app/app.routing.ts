366f681043c1489ab7d75c950789e7b6f3afba5b
Sergio <sergioh.c@live.com>
2018-12-21 17:33:09 -0200

    adjust routes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,10 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginComponent } from './users/login/login.component';
+import { RegisterComponent } from './users/register/register.component';
 
 const appRoutes: Routes = [
     { path: '', pathMatch: 'full', component: HomeComponent },
     { path: 'not-found', component: NotFoundComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
     { path: '**', redirectTo: 'not-found' }
 ];
 
