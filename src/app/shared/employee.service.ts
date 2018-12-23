0b29dc6143d05c858b908ae53d2e8d895c285c56
Sergio <sergioh.c@live.com>
2018-12-23 01:53:19 -0200

    service employee

--- src/app/shared/employee.service.ts
+++ src/app/shared/employee.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Angular2TokenService, A2tUiModule} from 'angular2-token';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class EmployeeService {
 
-  constructor() { }
+  constructor(private http: Http, private _tokenService: Angular2TokenService) { }
+
+  getEmployees() {
+    return this._tokenService.get('employees').map(res => res.json());
+  }
+
+
+  createEmployees(employee_params) {
+    return this._tokenService.post('employees', employee_params).map(res => res.json());
+  }
+
+  updateEmployees(employee_id, employee_params) {
+    return this._tokenService.put('employees/' + employee_id, employee_params).map(res => res.json());
+  }
+
+
+  destroyEmployees(employee_id) {
+    return this._tokenService.delete('employeess/' + employee_id).map(res => res.json());
+  }
 }
