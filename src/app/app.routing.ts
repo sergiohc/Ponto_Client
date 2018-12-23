a47a571a4fcbf80f1744c0aee2dda77e7d85f093
Sergio <sergioh.c@live.com>
2018-12-23 00:38:23 -0200

    add service employee

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,18 +1,21 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { UsersModule } from './users/users.module';
+import { EmployeesModule } from './employees/employees.module';
 
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { UsersModule } from './users/users.module';
 import { LoginComponent } from './users/login/login.component';
 import { RegisterComponent } from './users/register/register.component';
+import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
 
 const appRoutes: Routes = [
     { path: '', pathMatch: 'full', component: HomeComponent },
     { path: 'not-found', component: NotFoundComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'employees', component: EmployeeListComponent },
     { path: '**', redirectTo: 'not-found' }
 ];
 
