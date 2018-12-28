90a6facfc2b09a5b0499e3c09457803fb1ad876a
Sergio <sergioh.c@live.com>
2018-12-28 03:15:51 -0200

    add moment for format date grid

--- src/app/employees/clock-in-out-list/day/day.component.ts
+++ src/app/employees/clock-in-out-list/day/day.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ClockInOutService } from '../../../shared/clock-in-out.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import * as moment from 'moment';
 @Component({
   selector: 'app-day',
   templateUrl: './day.component.html',
@@ -16,11 +17,26 @@ export class DayComponent implements OnInit {
   rowData = [];
 
   columnDefs = [
-    {headerName: 'Data', field: 'date'},
-    {headerName: 'Entrada', field: 'hour1'},
-    {headerName: 'Intervalo', field: 'hour2'},
-    {headerName: 'Volta Intervalo', field: 'hour3'},
-    {headerName: 'Saída', field: 'hour4'}
+    {headerName: 'Data', field: 'date',
+    cellRenderer: (data) => {
+      return moment(data.date).format('MM/DD/YYYY')
+    }},
+    {headerName: 'Entrada', field: 'hour1',
+    cellRenderer: (data) => {
+      return moment(data.hour1).format('HH:mm')
+    }},
+    {headerName: 'Intervalo', field: 'hour2',
+      cellRenderer: (data) => {
+      return moment(data.hour2).format('HH:mm')
+    }},
+    {headerName: 'Volta Intervalo', field: 'hour3',
+      cellRenderer: (data) => {
+      return moment(data.hour3).format('HH:mm')
+    }},
+    {headerName: 'Saída', field: 'hour4',
+    cellRenderer: (data) => {
+      return moment(data.hour4).format('HH:mm')
+    }},
   ];
 
   ngOnInit() {
