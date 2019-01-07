4676ff4be762e1327842c2667cc975b86a4742ae
Sergio <sergioh.c@live.com>
2019-01-07 08:27:25 -0200

    fix

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/ponto-client'));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/ponto-client/src/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 // default Heroku port
